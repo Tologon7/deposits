cdb23546e6d69edd753f534551e53312a1dee00f
tologonkubandykov <marmokk@icloud.com>
2024-11-05 01:13:23 +0600

    created a detail view + update

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('list/', BlogListView.as_view(), name='blog-list'),
-    path('create/',BlogCreateView.as_view(), name='blog-create')
+    path('create/', BlogCreateView.as_view(), name='blog-create'),
+    path('<int:pk>/', BlogIdView.as_view(), name='blog-detail-update')
 
 ]
