083cb7e480a4f2071aa8d0bbd5e316ff264fbf01
Tologon7 <139872600+Tologon7@users.noreply.github.com>
2024-11-21 11:02:49 +0600

    optimize for deploy

--- depo/settings.py
+++ depo/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
-import certifi
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
